97f9a65dfd69d6f16ae827b20086796c5c57e457
ponraj123 <ponraj.bluesky@gmail.com>
2016-06-07 09:38:48 +0530

    Create eve.c

---
+++ eve.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("The no is even no:");
+}
+else if(n<0)
+{
+printf("The no is odd no:");
+}
+else
+{
+printf("The no is equal to zero:");
+}
+}
